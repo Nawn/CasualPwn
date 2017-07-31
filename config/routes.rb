0bdc3b62df9c269be60ee85620a13efed167f85d
Raul Gonzalez <Nawn1994@gmail.com>
2017-07-30 22:05:06 -0700

    Prepare router for new

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root 'pages#home'
 
+  get "user/new" => "users#prep"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
