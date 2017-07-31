485da6e7d7e685f1531fa14752b836164112049c
Raul Gonzalez <Nawn1994@gmail.com>
2017-07-30 22:58:01 -0700

    user/new will respond with create URL only when bot_api_key is correct

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,20 @@ class UsersController < ApplicationController
     if auth_bot(new_user_params[0])
       @new_guild_member = GuildMember.create(discord_id: new_user_params[1], discord_nick: new_user_params[2], confirm_token: rand_token)
 
-      render plain: "#{root_url}"
+      render plain: "#{root_url}#{user_create_path[1..-1]}?confirm_token=#{@new_guild_member.confirm_token}"
     else
       render :nothing => true, :status => :forbidden
     end
+  end
+
+  def assign_guild_user
+    
   end
 
   private
   def get_prep_user_params
     params.require([:bot_key, :discord_id, :discord_nick])
   end
+
+  def 
 end
