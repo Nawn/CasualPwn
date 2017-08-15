1ab0d3d6ca56f8cefc7142e035f01749092fc922
Raul Gonzalez <Nawn1994@gmail.com>
2017-08-14 22:07:50 -0700

    Begin some progress handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,21 @@ class UsersController < ApplicationController
     # Index 0 is the Bot API key
     if auth_bot(new_user_params[0])
       begin
-        @new_guild_member = GuildMember.create(discord_id: new_user_params[1], discord_nick: new_user_params[2], confirm_token: rand_token)
+        @new_guild_member = GuildMember.create(discord_id: new_user_params[1], discord_nick: new_user_params[2], confirm_token: rand_token, registration_progress: 0)
         render plain: "#{root_url}#{user_create_path[1..-1]}?confirm_token=#{@new_guild_member.confirm_token}"
       rescue ActiveRecord::RecordNotUnique => @e
+        # Continue onto the next step, after checking registration progress
+        @existing_guild_member = GuildMember.find_by(discord_id: new_user_params[1])
+
+        if @existing_guild_member.registration_progress == 0
+          render plain: "#{root_url}#{user_create_path[1..-1]}?confirm_token=#{@existing_guild_member.confirm_token}"
+        elsif @existing_guild_member.registration_progress == 1
+          # At this point, the Guild wars 2 Account has already been applied, so we should send out a message to the user
+          # To create a username, and a password
+
+          
+        end
+        
         render plain: "USER EXISTS"
       end
     else
