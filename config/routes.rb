5b779077ae526d5ba79f51274e85cf1be46bba03
Raul Gonzalez <Nawn1994@gmail.com>
2017-10-06 21:17:13 -0700

    Add posts route

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,9 @@ Rails.application.routes.draw do
     patch 'password/:id' => 'users#password_update'
   end
 
+  scope '/post' do
+    get 'new' => 'posts#new'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
