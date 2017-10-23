ffc8c14a246aaf9bff9220264c327eaae4ea08d4
Raul Gonzalez <Nawn1994@gmail.com>
2017-10-22 20:30:21 -0700

    Add GuildMember.update_ranks

--- app/models/guild_member.rb
+++ app/models/guild_member.rb
@@ -18,7 +18,40 @@ class GuildMember < ApplicationRecord
 
 	def self.update_ranks
 		our_guild = GuildWars::Guild.new(pull_from_global("guild_id"), pull_from_global("guild_leader_api"))
+		our_bot = Discord::Bot.new(pull_from_global('discord_token'), pull_from_global('discord_client').to_i)
 
-		our_guild.members
+		rank_reference = Hash.new
+
+		# Cross reference the name of the rank with the actual Integer
+		# need rank_reference to store that data
+		our_guild.ranks.each do |rank|
+			rank_reference[rank['id']] = rank['order']						
+		end
+
+		# Cross Reference for Rank to Discord Role ID (To set Role in Discord)
+		rank_to_discord = Hash.new
+		rank_to_discord[1] = 337060888068751363
+		rank_to_discord[2] = 337061298233933824
+		rank_to_discord[3] = 337061672940339201
+		rank_to_discord[4] = 337062083654844418
+		rank_to_discord[5] = 337062826801758210
+
+		gw2_users = our_guild.members
+
+		GuildMember.all.each do |site_member|
+			# Account must be complete to have Rank updated 
+			next unless site_member.registration_progress > 1
+			
+			# Find the appropriate guild wars User,
+			# Grab the rank, and run it through my reference to find the Rank Order Integer
+			my_gw2_user = gw2_users.find {|gw2_user_hash| gw2_user_hash['name'] == site_member.guild_wars_username }
+			my_gw2_rank = my_gw2_user['rank']
+
+			my_rank_int = rank_reference[my_gw2_rank]
+
+			site_member.update!(guild_rank: my_rank_int.to_i)
+
+			our_bot.update_permission(site_member.discord_id, rank_to_discord[my_rank_int])
+		end
 	end
 end
--- lib/discord_bot.rb
+++ lib/discord_bot.rb
@@ -20,5 +20,16 @@ module Discord
 
 			bot.send_message(pull_from_global('discord_announcements').to_i, "TITLE: `#{title}`\n```html\n#{ActionController::Base.helpers.strip_tags(content)}\n```")
 		end
+
+		def update_permission(discord_id, role_id)
+			bot = Discordrb::Bot.new token: @api_key, client_id: @client_id
+
+			bot.run :async
+			discord_server = bot.servers[pull_from_global('discord_guild_server').to_i]
+			role_to_set = discord_server.role(role_id)
+			member_to_update = discord_server.member(discord_id.to_i)
+
+			member_to_update.roles = role_to_set
+		end
 	end
 end
