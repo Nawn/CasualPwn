25f04d4e0e2d68307af941539a78bda21e84b17b
Raul Gonzalez <Nawn1994@gmail.com>
2017-10-22 17:53:40 -0700

    Begin gathering Rank data

--- app/models/guild_member.rb
+++ app/models/guild_member.rb
@@ -1,4 +1,24 @@
+require "#{Rails.root}/lib/guild_wars_call.rb"
+
+def pull_from_global(tag)
+	result = GlobalSetting.find_by(tag: tag)
+
+	if result.nil?
+	  return "ERROR: tag #{tag} not found in GlobalSetting"
+	else
+	  return result.content
+	end
+end
+
 class GuildMember < ApplicationRecord
+	include ApplicationHelper
+
 	has_secure_password :validations => false
 	has_many :posts
+
+	def self.update_ranks
+		our_guild = GuildWars::Guild.new(pull_from_global("guild_id"), pull_from_global("guild_leader_api"))
+
+		our_guild.members
+	end
 end
