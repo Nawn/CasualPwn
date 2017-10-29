520f1f7ed3e8504ce0b3c6c659622564cef1957f
Raul Gonzalez <Nawn1994@gmail.com>
2017-10-29 13:50:22 -0700

    Program the date picker

--- app/controllers/guild_events_controller.rb
+++ app/controllers/guild_events_controller.rb
@@ -1,9 +1,13 @@
 class GuildEventsController < InheritedResources::Base
+  
+  def new
+  	@guild_event = GuildEvent.new(start_time: Time.current, end_time: Time.current)
+  end
 
   private
 
     def guild_event_params
-      params.require(:guild_event).permit(:start_time, :end_time, :title, :description, :category, :guild_only, :roster_size)
+      params.require(:guild_event).permit(:start_time_time, :start_time_date, :end_time_time, :end_time_date, :title, :description, :category, :guild_only, :roster_size)
     end
 end
 
--- app/models/guild_event.rb
+++ app/models/guild_event.rb
@@ -1,2 +1,52 @@
 class GuildEvent < ApplicationRecord
+	before_save :combine_date_time
+
+	def start_time_time
+		puts "Sending Start Time"
+		self.start_time.strftime("%l:%M %p") if self.start_time.present?
+	end
+
+	def start_time_date
+		puts "Sending Start Date"
+		self.start_time.strftime("%e %B, %Y") if self.start_time.present?
+	end
+
+	def start_time_time=(time)
+		puts "Setting Start Time"
+		@start_time_time = Time.zone.parse(time).getutc.strftime("%H:%M:%S")
+	end
+
+	def start_time_date=(date)
+		puts "Setting Start Date"
+		@start_time_date = Date.parse(date).strftime("%Y-%m-%d")
+	end
+
+	def end_time_time
+		puts "Sending End Time"
+		self.end_time.strftime("%l:%M %p") if self.end_time.present?
+	end
+
+	def end_time_date
+		puts "Sending End Date"
+		self.end_time.strftime("%e %B, %Y") if self.end_time.present?
+	end
+
+	def end_time_time=(time)
+		puts "Setting End Time"
+		@end_time_time = Time.zone.parse(time).getutc.strftime("%H:%M:%S")
+	end
+
+	def end_time_date=(date)
+		puts "Setting End Date"
+		@end_time_date = Date.parse(date).strftime("%Y-%m-%d")
+	end
+
+	def combine_date_time
+		puts "Combining Start Times to DateTime"
+		puts "Start Date: #{@start_time_date} Start Time: #{@start_time_time}"
+		puts "End Date: #{@end_time_date} End Time: #{@end_time_time}"
+		self.start_time = DateTime.parse("#{@start_time_date} #{@start_time_time}")
+		self.end_time = DateTime.parse("#{@end_time_date} #{@end_time_time}")
+	end
+
 end
