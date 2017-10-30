75df48a2fd48bc5a1a5f6f41484cf603ad0b0c3f
Raul Gonzalez <Nawn1994@gmail.com>
2017-10-30 13:34:02 -0700

    Create Link Generators

--- app/helpers/guild_events_helper.rb
+++ app/helpers/guild_events_helper.rb
@@ -1,2 +1,9 @@
 module GuildEventsHelper
+	def event_link(event, member)
+		if event.roster.include? member.id
+			link_to "Dropout", guild_event_leave_path(event)
+		else
+			link_to "Signup", guild_event_join_path(event)
+		end
+	end
 end
